1f65a96fd95003d0926e070d4a60945e460199a0
CaedenPH <caedenperelliharris@gmail.com>
2022-03-26 01:26:51 +0000

    fix: Use static css files

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const ejs = require('ejs');
 const express = require('express');
 const app = express();
 
-
 app.set("view engine", "ejs")
+app.use('/static', express.static(__dirname + '/static'))
+
+
 app.get('/', async (req, res) => {
     res.render('index', {
-        name: await sys.cpu()
+        brand: await (await sys.cpu()).brand
     });
 });
 
