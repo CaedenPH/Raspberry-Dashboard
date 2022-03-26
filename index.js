c1125710e6eaebd3d51ca24b48a510d17eceed0e
CaedenPH <caedenperelliharris@gmail.com>
2022-03-26 17:08:31 +0000

    feat: Reboot and signin path

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const sys = require('systeminformation');
-const ejs = require('ejs');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const auth = require('./auth.js');
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+const exec = require('child_process').exec;
+const { password } = require('./config.json')
 
-app.set("view engine", "ejs")
-app.use('/static', express.static(__dirname + '/static'))
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.use('/static', express.static(__dirname + '/static'));
+app.use(auth);
+
 
 
 app.get('/', async (req, res) => {
     res.render('index', {
         brand: await (await sys.cpu()).brand
     });
+});
+
+app.get("/signin", async (req, res) => {
+    res.render('signin');
+});
+
+app.post("/signin", async (req, res) => {
+    if (req.body.password && req.body.password === password) {
+        res.cookie("_ashoisdhiozvsb", jwt.sign({
+            username: "test123"
+        }, "aoihfisoduhgoiahusSECRET_KEY", {
+            expiresIn: 10800
+        }), {
+            httpOnly: true,
+            secure: true,
+            expiresIn: 10800
+        });
+        res.status(200).json({ message: "s" });
+    } else {
+        res.status(400).json({ message: "Bad Argument" });
+    }
+});
+
+app.get("/reboot", async (req, res) => {
+    exec('reboot')
 });
 
 
