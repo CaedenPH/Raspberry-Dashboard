037085e1b0c2b43154c83996cfb5befda6d113cf
CaedenPH <caedenperelliharris@gmail.com>
2022-04-05 20:16:53 +0100

    feat: Websocket and client with full reconnection

--- connection.py
+++ connection.py
@@ -1,10 +1,67 @@
+from __future__ import annotations
+
 import aiohttp
 import asyncio
+import json
+
+
+class DisconnectError(Exception):
+    """Raised when the websocket is forcebly disconnected from the server."""
+
+
+class WebSocket:
+    REQUEST = 0
+    RESPONSE = 1
+    IDENTIFY = 2
+
+    def __init__(self, ws: aiohttp.ClientWebSocketResponse, client: Client) -> None:
+        self.socket = ws
+        self.client = client
+
+    async def identify(self) -> None:
+        with open("config.json") as stream:
+            data: dict = json.load(stream)
+
+        await self.socket.send_json({
+            "op": self.IDENTIFY, 
+            "token": data.get("ws_token")
+        })
+    
+    async def listen(self) -> None:
+        await self.identify()
+        async for message in self.socket:
+            print(message)
+        raise DisconnectError
+
+
+    @classmethod
+    async def connect(cls, client: Client) -> WebSocket:
+        ws = await client._session.ws_connect("ws://localhost:8080")
+        return cls(ws, client)
+
+
+class Client:
+    def __init__(self) -> None:
+        self._session = aiohttp.ClientSession()
+    
+    async def ws_connect(self) -> WebSocket | bool:
+        try:
+            self._ws = await WebSocket.connect(self)
+            return self._ws
+        except aiohttp.ClientConnectionError:
+            return False
+
 
 async def main() -> None:
-    client = aiohttp.ClientSession()
-    connection = await client.ws_connect("ws://localhost:8080")
-    async for message in connection:
-        print(message)
+    client = Client()
 
+    while True:
+        connection = await client.ws_connect()
+        if connection is not False:
+            try:
+                await connection.listen()
+            except DisconnectError:
+                continue
+                
+        
 asyncio.run(main())
