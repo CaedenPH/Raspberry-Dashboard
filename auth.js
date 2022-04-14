f8194a864d7367b7f7621dd1d6ac7244b112f668
CaedenPH <caedenperelliharris@gmail.com>
2022-04-14 22:38:31 +0100

    feat: Dynamic cookies and explicit logins

--- auth.js
+++ auth.js
@@ -1,11 +1,19 @@
+const cookie_value = require("./config.json").cookie_value;
 const jwt = require("jsonwebtoken");
 
 module.exports = (request, response, next) => {
     if (["/", "/statistics", "/logs", "/signin"].includes(request.path)) {
         next();
+    } else if (["/protocols", "/reset"].includes(request.path)) {
+        try {
+            jwt.verify(request.cookies["_fiojoweonfwouinwiunfuiw"] || "", "aoihfisoduhgoiahusSECRET_KEY");
+            next();
+        } catch (err) {
+            response.redirect("/explicit");
+        }
     } else {
         try {
-            jwt.verify(request.cookies._ashoisdhiozvsb || "", "aoihfisoduhgoiahusSECRET_KEY");
+            jwt.verify(request.cookies[cookie_value] || "", "aoihfisoduhgoiahusSECRET_KEY");
             next();
         } catch (err) {
             response.redirect("/signin");
