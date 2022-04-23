f27bbc978f262d05a875ef6d3e250043d107177e
CaedenPH <caedenperelliharris@gmail.com>
2022-04-23 20:21:30 +0100

    feat: Log everytime someone visits the website

--- auth.js
+++ auth.js
@@ -1,12 +1,15 @@
 const cookie_value = require("./config.json").cookie_value;
 const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 module.exports = (request, response, next) => {
     if (["/", "/statistics", "/logs", "/login", "/jesterbot", "/stealthybot", "/dashboard", "/storage"].includes(request.path)) {
+        fs.appendFileSync("logs/usage.txt", `${request.path} | ${request.ip} | ${request.protocol} | ${new Date().toUTCString()}\n`)
         next();
     } else if (["/protocols", "/reset"].includes(request.path)) {
         try {
             jwt.verify(request.cookies["_fiojoweonfwouinwiunfuiw"] || "", "aoihfisoduhgoiahusSECRET_KEY");
+            fs.appendFileSync("logs/usage.txt", `${request.path} | ${request.ip} | ${request.protocol} | ${new Date().toUTCString()}\n`)
             next();
         } catch (err) {
             response.redirect("/explicit");
@@ -14,6 +17,7 @@ module.exports = (request, response, next) => {
     } else {
         try {
             jwt.verify(request.cookies[cookie_value] || "", "aoihfisoduhgoiahusSECRET_KEY");
+            fs.appendFileSync("logs/usage.txt", `${request.path} | ${request.ip} | ${request.protocol} | ${new Date().toUTCString()}\n`)
             next();
         } catch (err) {
             response.redirect("/login");
