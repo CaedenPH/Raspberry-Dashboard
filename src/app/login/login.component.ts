2abdaa8319b71e840d5a1c70e470674e1722a6e1
realstealthninja <realstealthninja@gmail.com>
2024-02-05 21:44:10 +0530

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  authForm: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.authForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  /**
+   * login method
+   */
+  login() {
+    const value = this.authForm.value;
+
+    if (value.email && value.password) {
+      this.authService.login(value.email, value.password).subscribe(() => {
+        this.router.navigateByUrl('/');
+      });
+    }
+  }
 }
