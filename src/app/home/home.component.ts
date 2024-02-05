9f1e4a1dc9371296019138912a3c7d5d05510649
realstealthninja <realstealthninja@gmail.com>
2024-02-05 21:44:01 +0530

    adding test for login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [LoginComponent],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
